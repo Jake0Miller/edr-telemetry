33c7e959576e1ee940e13718ea40d9fbd95bd07a
Jake Miller <jake0miller@gmail.com>
2020-10-14 00:53:48 -0600

    Add exception handling to udp controller

--- utils/delete_controller.py
+++ utils/delete_controller.py
@@ -8,8 +8,9 @@ def prune_subdirectories():
     locs = []
     for path, subdirs, files in os.walk(FILES_DIR):
         for subdir in subdirs:
-            base = path.split('edr-telemetry/')
-            loc = base[1] + '/' + subdir
+            dir_name = os.getcwd().split('/')[-1]
+            base = path.split(dir_name + '/')[-1]
+            loc = base + '/' + subdir
             locs.insert(0, loc)
             if not os.listdir(loc):
                 os.rmdir(loc)
